08dd9c64e5d37d5be963be6dd5523fc4f6e2f3d1
James Shelton <jshelton140@googlemail.com>
2022-06-29 01:48:12 +0100

    Add friend manager and plugin core logic

--- src/main/java/com/friendtracker/FriendTrackerPlugin.java
+++ src/main/java/com/friendtracker/FriendTrackerPlugin.java
@@ -1,19 +1,34 @@
 package com.friendtracker;
 
-import com.friendtracker.io.SaveManager;
+import com.friendtracker.data.FriendDataClient;
+import com.friendtracker.data.TrackerDataStore;
+import com.friendtracker.friends.Friend;
+import com.friendtracker.friends.FriendManager;
 import com.friendtracker.panel.FriendTrackerPanel;
+import com.friendtracker.panel.MergePanel;
 import com.google.inject.Provides;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 import javax.inject.Inject;
+import javax.swing.SwingUtilities;
+import jdk.nashorn.internal.runtime.regexp.joni.exception.ValueException;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
+import net.runelite.api.GameState;
+import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.hiscore.HiscoreResult;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.ClientToolbar;
 import net.runelite.client.ui.NavigationButton;
 import net.runelite.client.util.ImageUtil;
-import okhttp3.OkHttpClient;
 
 @Slf4j
 @PluginDescriptor(
@@ -22,25 +37,23 @@ import okhttp3.OkHttpClient;
 public class FriendTrackerPlugin extends Plugin
 {
 
-	@Inject
-	private Client client;
-
-	@Inject
-	private FriendTrackerConfig config;
-
-	@Inject
-	private ClientToolbar clientToolbar;
-
-	private SaveManager saveManager;
+	public static final String CONFIG_GROUP_NAME = "friend-tracker";
+	@Inject private Client client;
+	@Inject private ConfigManager configManager;
+	@Inject private FriendTrackerConfig config;
+	@Inject private FriendDataClient friendDataClient;
+	@Inject private TrackerDataStore trackerDataStore;
+	@Inject private ClientToolbar clientToolbar;
 
 	private FriendTrackerPanel panel;
 	private NavigationButton navButton;
+	@Getter private FriendManager friendManager;
+
 
 	@Override
 	protected void startUp() throws Exception
 	{
-		saveManager = new SaveManager(client);
-		panel = new FriendTrackerPanel(client, this, config, saveManager, new OkHttpClient());
+		panel = new FriendTrackerPanel(client, this, config);
 
 		final BufferedImage icon = ImageUtil.loadImageResource(getClass(), "panel_icon.png");
 
@@ -60,23 +73,165 @@ public class FriendTrackerPlugin extends Plugin
 		clientToolbar.removeNavigation(navButton);
 	}
 
-	public void refreshList()
-	{
-		net.runelite.api.Friend[] friendList = this.client.getFriendContainer().getMembers();
+	private long lastAccount;
 
-		panel.reset();
+	@Subscribe
+	public void onGameStateChanged(GameStateChanged event) {
+		GameState state = event.getGameState();
 
-		for(net.runelite.api.Friend friend:friendList)
-		{
-			panel.lookup(friend.getName());
+		if (state == GameState.LOGGED_IN) {
+			// LOGGED_IN is triggered between region changes too.
+			// Check that the RuneScape account has changed
+			if (client.getAccountHash() != lastAccount) {
+				// Reset
+				log.debug("Account change: {} -> {}",
+						lastAccount, client.getAccountHash());
+
+				friendManager = new FriendManager(client.getAccountHash());
+
+				trackerDataStore.getFriendDataFromConfig(client.getAccountHash())
+						.ifPresent(friendManager::applySaveData);
+
+				lastAccount = client.getAccountHash();
+			}
 		}
 
-		panel.rebuild();
+		SwingUtilities.invokeLater(() -> panel.setLoggedIn(isLoggedInState(state)));
+	}
+
+	private boolean isLoggedInState(GameState gameState)
+	{
+		return gameState != GameState.LOGIN_SCREEN && gameState != GameState.LOGIN_SCREEN_AUTHENTICATOR;
+	}
+
+	public void refreshList()
+	{
+		net.runelite.api.Friend[] friendNames = this.client.getFriendContainer().getMembers();
+
+		List<Friend> currentFriends = new ArrayList<>(friendManager.getFriends().values());
+
+		for(net.runelite.api.Friend friendName : friendNames)
+		{
+			Friend friend = new Friend(UUID.randomUUID().toString(), friendName.getName(), friendName.getPrevName());
+
+			log.info("Fetching HiscoreResult for " + friendName.getName());
+			friendDataClient.lookupAsync(friendName.getName()).whenCompleteAsync((result, exception) ->
+			{
+				if(result == null || exception != null)
+				{
+					StringBuilder stringBuilder = new StringBuilder("Hiscore lookup for \"" + friendName.getName() + "\" failed");
+					if(exception != null) stringBuilder.append(" with exception: ").append(exception.getMessage());
+
+					log.warn(stringBuilder.toString());
+					return;
+				}
+
+				friend.addSnapshotNow(result);
+
+				List<Friend> mergeCandidates = friendManager.getValidMergeCandidates(friend);
+
+				if(mergeCandidates.isEmpty() || currentFriends.isEmpty())
+				{
+					SwingUtilities.invokeLater(() ->
+					{
+						resolveMerge(friend, "");
+						log.info("No valid merge candidates. Added without merge: {}", friend.getName());
+					});
+					return;
+				}
+
+				if(mergeCandidates.size() == 1)
+				{
+					SwingUtilities.invokeLater(() ->
+					{
+						resolveMerge(friend, mergeCandidates.get(0).getID());
+						log.info("One valid merge candidate. Merged {} into {}. ", friend.getName(), mergeCandidates.get(0).getName());
+					});
+					return;
+				}
+
+				SwingUtilities.invokeLater(() ->
+				{
+					queryMerge(friend, mergeCandidates);
+					log.info("Merge query called for {}.", friend.getName());
+				});
+			});
+
+		}
+
+		SwingUtilities.invokeLater(() -> panel.redraw());
+	}
+
+	/**
+	 *
+	 * @param baseFriend
+	 * @param candidates
+	 */
+	public void queryMerge(Friend baseFriend, List<Friend> candidates)
+	{
+		MergePanel mergePanel = new MergePanel(this, config, baseFriend, candidates);
+		panel.addMergePanel(mergePanel);
+	}
+
+	/**
+	 *
+	 * @param newFriend
+	 * @param mergeTargetID
+	 */
+	public void resolveMerge(Friend newFriend, String mergeTargetID)
+	{
+		if(mergeTargetID.equals(""))
+		{
+			friendManager.add(newFriend);
+		}
+		else
+		{
+			friendManager.merge(newFriend, mergeTargetID);
+		}
+
+		saveCurrentFriendData();
+
+		SwingUtilities.invokeLater(() -> panel.redraw());
+	}
+
+	public void nextMergePanel()
+	{
+		SwingUtilities.invokeLater(() -> panel.drawNextMergePanel());
+	}
+
+	public void saveCurrentFriendData()
+	{
+		trackerDataStore.saveFriendDataToConfig(friendManager.getAccountHash(), friendManager.getFriends());
 	}
 
 	@Provides
 	FriendTrackerConfig provideConfig(ConfigManager configManager)
 	{
 		return configManager.getConfig(FriendTrackerConfig.class);
+	}
+
+	//@todo debug method to be removed
+	private void readConfig()
+	{
+		Optional<Map<String, Friend>> friendDataOptional = trackerDataStore.getFriendDataFromConfig(client.getAccountHash());
+
+		if(friendDataOptional.isPresent())
+		{
+			Friend elixir = friendDataOptional.get().values().stream()
+					.filter(friend -> friend.getName().equalsIgnoreCase("Elixir140"))
+					.findAny()
+					.orElse(null);
+
+			if(elixir == null) throw new NullPointerException("Friend Elixir not found.");
+
+			HiscoreResult result = elixir.getHiscoreSnapshots().lastEntry().getValue();
+
+			if(result.getOverall().getLevel() != 2123) throw new ValueException("Overall level incorrect."); else log.info("Overall level deserialized correctly.");
+			if(result.getVorkath().getLevel() != 2277) throw new ValueException("Vorkath kc incorrect."); else log.info("Vorkath kc deserialized correctly.");
+		}
+		else
+		{
+			log.warn("Optional returned empty.");
+		}
 	}
 }
--- src/main/java/com/friendtracker/friends/FriendManager.java
+++ src/main/java/com/friendtracker/friends/FriendManager.java
@@ -1,17 +1,69 @@
 package com.friendtracker.friends;
 
-import com.friendtracker.data.FriendDataClient;
+import com.friendtracker.FriendTrackerPlugin;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
+@Data
+@Slf4j
 public class FriendManager {
 
-    private final FriendDataClient friendDataClient;
     private final long accountHash;
-    private Map<String, Friend> friends = new HashMap<>();;
+    private Map<String, Friend> friends = new HashMap<>(); //Key: UUID.randomUUID().toString();
 
-    public FriendManager(long accountHash, FriendDataClient friendDataClient) {
+    public FriendManager(long accountHash)
+    {
         this.accountHash = accountHash;
-        this.friendDataClient = friendDataClient;
+    }
+
+    /** Populates an empty FriendManager with the given collection of Friends.
+     * Returns without changing the state of this FriendManager if it is not empty.
+     * Use {@link FriendManager#add(Friend)} to add Friends to a non-empty FriendManager.
+     *
+     * @param friends the Friend objects to populate this FriendManager
+     */
+    public void applySaveData(Map<String, Friend> friends)
+    {
+        // Only apply save data if friends list is empty
+        if(!this.friends.isEmpty())
+        {
+            log.error("Cannot apply save data to a FriendManager that already has Friends. Use addOrMerge() instead.");
+            return;
+        }
+
+        this.friends.putAll(friends);
+
+        log.info(this.friends.values().stream().map(Friend::getName).collect(Collectors.joining(", ")));
+    }
+
+    public void add(Friend friend)
+    {
+        friends.put(friend.getID(), friend);
+
+        log.info(this.friends.values().stream().map(Friend::getName).collect(Collectors.joining(", ")));
+    }
+
+    public void merge(Friend newFriend, String mergeTargetID)
+    {
+        friends.get(mergeTargetID).merge(newFriend);
+
+        log.info(this.friends.values().stream().map(Friend::getName).collect(Collectors.joining(", ")));
+    }
+
+    public List<Friend> getValidMergeCandidates(Friend newFriend)
+    {
+        List<Friend> mergeCandidates = new ArrayList<>();
+
+        for(Friend friend : friends.values())
+        {
+            if(friend.isValidToMerge(newFriend)) mergeCandidates.add(friend);
+        }
+
+        return mergeCandidates;
     }
 }
